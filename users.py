535bc11f00d83a64759314a434005fec7d331e62
Arnoldblair <arnoldblair.munene@student.moringaschool.com>
2022-01-27 14:03:11 +0300

    added users

---
+++ users.py
@@ -0,0 +1,48 @@
+import unittest
+from users import User
+
+class TestPassword(unittest.TestCase):
+    '''
+    Test class that defines test cases for the Password class behaviours
+    '''
+
+    def setUp(self):      
+        
+        
+        self.new_users = User("Mbugua","jmos849")
+       
+
+
+    def tearDown(self):
+        '''
+        Tear down method that does clean up after each test case has run
+        '''
+        User.users_list = []
+        
+
+    def test__init(self):
+        '''
+        Test to check whether the credentials objects are instantiated correctly
+        '''
+        
+        self.assertEqual(self.new_users.user_name,"Blair")
+        self.assertEqual(self.new_users.user_passwrd,"munene12")
+
+    def test_save_users(self):
+        '''
+        Test to check if the object is saved into the users list
+        '''
+        self.new_users.save_user()
+        self.assertEqual(len(User.users_list),1)
+
+    # def test_authenticate_user(self):
+    #     '''
+    #     This method tests user authentication
+    #     '''
+    #     self.assertTrue(self.new_users.user_authenticate("Blair", "munene12"))
+
+  
+
+
+if __name__ == '__main__':
+    unittest.main()
